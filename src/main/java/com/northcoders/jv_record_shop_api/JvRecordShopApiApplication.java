3207b668ebee17c76e8700ad54cce997188e00a9
Joshua McDonagh <joshuamichaelmcdonagh@gmail.com>
2024-12-10 19:49:12 +0000

    start of project

---
+++ src/main/java/com/northcoders/jv_record_shop_api/JvRecordShopApiApplication.java
@@ -0,0 +1,13 @@
+package com.northcoders.jv_record_shop_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvRecordShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvRecordShopApiApplication.class, args);
+	}
+
+}
