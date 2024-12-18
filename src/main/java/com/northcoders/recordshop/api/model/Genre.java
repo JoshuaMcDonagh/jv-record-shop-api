bdf5efd44a56202a482b1ccbdb1692ac556844b3
Joshua McDonagh <joshuamichaelmcdonagh@gmail.com>
2024-12-18 18:22:43 +0000

    feat: add Genre enum for use in album entity

---
+++ src/main/java/com/northcoders/recordshop/api/model/Genre.java
@@ -0,0 +1,13 @@
+package com.northcoders.recordshop.api.model;
+
+public enum Genre {
+    ALTERNATIVE,
+    ROCK,
+    COUNTRY,
+    HIP_HOP,
+    JAZZ,
+    POP,
+    RAP,
+    METAL,
+    HEAVY_METAL
+}
