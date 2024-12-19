d6440269eb467eb83c1fbd25fc90ebe00e8cd693
Joshua McDonagh <joshuamichaelmcdonagh@gmail.com>
2024-12-19 12:05:46 +0000

    feat: add Album class as entity

---
+++ src/main/java/com/northcoders/recordshop/api/model/Album.java
@@ -0,0 +1,50 @@
+package com.northcoders.recordshop.api.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name="albums")
+@Data
+public class Album {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @ManyToMany
+    @JoinTable(
+            name="album_genre",
+            joinColumns=@JoinColumn(name="album_id"),
+            inverseJoinColumns=@JoinColumn(name="genre"),
+            uniqueConstraints=@UniqueConstraint(columnNames={"album_id", "genre"})
+    )
+    @Column(nullable = false)
+    private Set<GenreEntity> genres = new HashSet<>();
+
+    @ManyToMany
+    @JoinTable(
+            name="album_artist",
+            joinColumns=@JoinColumn(name="album_id"),
+            inverseJoinColumns=@JoinColumn(name="artist_id"),
+            uniqueConstraints=@UniqueConstraint(columnNames={"album_id", "artist_id"})
+    )
+    @Column(nullable = false)
+    private Set<Artist> artists;
+
+    @Column(nullable = false)
+    private int yearReleased;
+
+    @Column(nullable = false)
+    private long stock;
+
+    @Column(nullable = false)
+    private double price;
+
+}
